c0b4df89f2b5a504fe06c2fb2dc7890019ea5639
anickel101 <anson.nickel@gmail.com>
2020-09-17 15:25:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+  function addSortButton() {
+    let list = document.getElementById("list");
+    let sortButton = document.createElement("div");
+    sortButton.innerHTML =
+    `<button type="button" id="sort-button">Sort by Priority</button>`;
+    list.append(sortButton);
+  }
+
+  addSortButton();
+
+
   // Click Listener to add task
   document.addEventListener("submit", function(e) {
     e.preventDefault();
@@ -8,18 +19,26 @@ document.addEventListener("DOMContentLoaded", () => {
     let taskList = document.getElementById("tasks");
     let newListItem = document.createElement("li");
     newListItem.classList.add("list-item");
+    newListItem["priority"] = 3;
     newListItem.innerHTML = 
     `<p class="task-text">${task.value}</p>
     <form> 
       <select name="priority" class="priority">
-        <option value="High"> High Priority </option>
-        <option value="Medium"> Medium Priority </option>
-        <option value="Low"> Low Priority </option>
+        <option value="1"> High Priority </option>
+        <option value="2"> Medium Priority </option>
+        <option value="3"> Low Priority </option>
       </select>
     </form>
     <button type="button" id="delete-button"> X </button>`
     taskList.append(newListItem);
     task.value = "";
+  })
+
+  //  Click Listener for Sort 
+  document.addEventListener('click', function(e) {
+    if (e.target.id === "sort-button") {
+      sortTasks();
+     }
   })
 
   //  Click Listener for Delete 
@@ -37,15 +56,47 @@ document.addEventListener("DOMContentLoaded", () => {
         let closestLi = e.target.closest(".list-item");
         let text = closestLi.querySelector(".task-text");
 
-        if (result === "Low") {
+        if (result === "3") {
+          closestLi["priority"] = 3;
           text.style.color = "green";
-        } else if (result === "Medium") {
+        } else if (result === "2") {
+          closestLi["priority"] = 2;
           text.style.color = "orange";
-        } else if (result === "High") {
+        } else if (result === "1") {
+          closestLi["priority"] = 1;
           text.style.color = "Red";
         }
     }
   })
+
+  function sortTasks(){
+
+    let old_ul = document.querySelector("#tasks");
+    console.log("Old UL");
+    console.log(old_ul);
+
+    let new_ul = old_ul.cloneNode(true);
+    console.log("New UL");
+    console.log(new_ul);
+
+    let lis = [];
+    for (let i = 0; i < old_ul.children.length; i++) {
+      if(old_ul.children[i].nodeName === "LI") {
+        lis.push(old_ul.children[i]);
+      }
+    }
+
+    lis.sort((a, b) => {
+      return a.priority - b.priority;
+    });
+
+    for(let i = 0; i < lis.length; i++) {
+      new_ul.appendChild(lis[i]);
+      old_ul.parentNode.replaceChild(new_ul, old_ul);
+    }
+
+
+  }
 
 
 });
