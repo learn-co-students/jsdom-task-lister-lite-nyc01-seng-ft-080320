9f2076a599804eb8a1dcc5296a5e7dc9e0398450
kevinpark07 <kevin.park07@yahoo.com>
2020-09-18 17:40:41 -0400

    sort working

--- src/index.js
+++ src/index.js
@@ -11,23 +11,65 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const dropDown = document.createElement('select')
 
+  let finalColor;
+
   function dropMenu() {
     const form = document.querySelector('#create-task-form');
     form.insertAdjacentElement('beforeend', dropDown);
+    createOptions("Choose Priority");
+    createOptions("High");
+    createOptions("Medium");
+    createOptions("Low");
   }
   
-  dropMenu();
+  const createOptions = priority => {
+    const option = document.createElement('option')
+    option.setAttribute("name", priority);
+    option.textContent = priority;
+    console.log(option)
+    dropDown.appendChild(option)
+  }
+
+ 
+  
+    dropDown.addEventListener('change', e => {
+      let color;
+      if (e.target.value === "High") {
+        color = "red"
+       } else if (e.target.value === "Medium") {
+        color =  "yellow"
+       } else if (e.target.value === "Low") {
+         color = "green"
+       }
+      console.log(color)
+      finalColor = color
+    })
+  
+    
+  
+  // const createLi = () => {
+  
+    
+  //   return color
+  // }
+
 //function clicker() {
  document.addEventListener("submit", function(e) {
-   const getTask = newTaskDescription.value;
+   
    const deleteButton = document.createElement('button')
    deleteButton.innerHTML = "Delete"
+   
    e.preventDefault();
-      function createTask() {
+
+    function createTask() {
         const taskLi = document.createElement('li')
+        const getTask = newTaskDescription.value;
         taskLi.innerHTML = getTask
+        taskLi.style.color = finalColor
+        //const newTaskLi = createLi()
         taskLi.append(deleteButton)
         tasks.append(taskLi)
+        //console.log(prioritySelect())
       }//createTask
     function removeTask() {
       deleteButton.addEventListener("click", function(e) {
@@ -35,9 +77,12 @@ document.addEventListener("DOMContentLoaded", () => {
        })//delete button
     }//remove task
     
-   
+    // createTaskForm.reset();
+    
     createTask();
     removeTask();
-    
+
   })//submit
+  dropMenu();
+  // prioritySelect();
 })//DOM Content
