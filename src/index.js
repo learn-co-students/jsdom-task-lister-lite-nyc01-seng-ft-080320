161f8df550c51df04e25d2fdd3af95087e1d9f1a
github username <github email address>
2020-09-19 20:29:28 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
+// your code here
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const SubmitHandler = () => {
+    document.addEventListener('submit', e => {
+      e.preventDefault()
+      const tasks = document.querySelector('#tasks')
+
+      const form = e.target
+
+      const taskLi = document.createElement('li')
+      const desc = form[0].value
+      taskLi.innerText = desc
+
+  
+
+      tasks.innerHTML = `
+      <span>${desc}</span>
+      <button>x</button>
+      `
+
+      tasks.append(taskLi);
+    })
+  }
+
+const clickHandler = () => {
+  document.addEventListener('click', e => {
+    if(e.target.matches('#btn')) {
+      const button = e.target
+      const buttonLi = button.parentElement
+      buttonLi.remove();
+    }
+  })
+}
+  
+
+    clickHandler();
+    SubmitHandler(); 
 });
